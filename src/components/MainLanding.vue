1d53e9b5f7c1cfac6711fc8cdbd1896f361f1f72
John Christian Los Bañes <jglosbanes@up.edu.ph>
2019-06-17 02:03:22 +0800

    Replace Bootstrap 4 with Bootstrap Vue

--- src/components/MainLanding.vue
+++ src/components/MainLanding.vue
@@ -2,6 +2,21 @@
     <div>
         <div id='particles-js'></div>
         <div id='login'>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h1 class="display-1 text-white text-nowrap">Decentralized Voting System</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="offset-sm-4 col-sm-2">
+                        <button type="button" class="btn btn-lg btn-outline-light btn-block">Log-in</button>
+                    </div>
+                    <div class="col-sm-2">
+                        <button type="button" class="btn btn-lg btn-outline-light btn-block">Register</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -29,8 +44,6 @@
 
 <style scoped>
     #particles-js {
-        width: 100%;
-        height: 100%;
         background: #000000de;
     }
 
@@ -42,5 +55,4 @@
         -webkit-transform: translateX(-50%) translateY(-50%);
         transform: translateX(-50%) translateY(-50%);
     }
-
 </style>
