3db7958126e7122af422f90a7d571f79c7cd040e
John Christian Los Bañes <jglosbanes@up.edu.ph>
2019-06-17 01:02:17 +0800

    Initialize App Landing Page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <MainLanding />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import MainLanding from './components/MainLanding.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            MainLanding,
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MainLanding.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div id='particles-js'></div>
+        <div id='login'>
+        </div>
+    </div>
+</template>
+
+<script>
+    require('particles.js')
+    import ParticleSettings from './../assets/particles.json';
+
+    export default {
+        name: 'ParticlesJS',
+
+        created() {
+            this.$nextTick(() => {
+                this.initParticlesJS()
+            })
+        },
+
+        methods: {
+            initParticlesJS() {
+                particlesJS('particles-js', ParticleSettings);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #particles-js {
+        width: 100%;
+        height: 100%;
+        background: #000000de;
+    }
+
+    #login {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -moz-transform: translateX(-50%) translateY(-50%);
+        -webkit-transform: translateX(-50%) translateY(-50%);
+        transform: translateX(-50%) translateY(-50%);
+    }
+
+</style>
