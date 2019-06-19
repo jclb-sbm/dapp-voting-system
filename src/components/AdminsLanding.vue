75e40cbaec489dee2f91e5363854305e28da63e4
John Christian Los Bañes <jglosbanes@up.edu.ph>
2019-06-19 17:52:03 +0700

    Admin Feature - Upload Picture to IPFS

--- src/components/AdminsLanding.vue
+++ src/components/AdminsLanding.vue
@@ -3,24 +3,33 @@
         <h1>Admins</h1>
 
         <form>
-            <input type="text" v-model="candidateName"/>
-            <button v-on:click.prevent ="registerCandidate">
+            <input type="text" v-model="candidateName" />
+            <button @click.prevent="registerCandidate">
                 Register Candidate
             </button>
-            <button v-on:click.prevent ="getCandidate">
+            <button @click.prevent="getCandidate">
                 Get Candidate
             </button>
-            <button v-on:click.prevent ="getVotes">
+            <button @click.prevent="getVotes">
                 Get Candidate Votes
             </button>
         </form>
+
+        <b-form-file plain @change="captureFile" />
+        <button @click.prevent="uploadPhoto">
+            Upload Photo
+        </button>
 
     </div>
 </template>
 
 <script>
     const Web3 = require('web3');
+    const IPFS = require('ipfs-http-client')
+
+    const Buffer = IPFS.Buffer;
     const web3 = new Web3('ws://localhost:8545', null, {});
+    const ipfs = new IPFS('/ip4/127.0.0.1/tcp/5001');
 
     export default {
         data() {
@@ -28,9 +37,11 @@
                 candidateName: null,
                 contract: null,
                 defaultAccount: null,
+
+                buffer: null,
             }
         },
-        mounted: async function() {
+        mounted: async function () {
             const VotingSystem = require('./../../build/contracts/VotingSystem.json');
 
             const contractABI = VotingSystem.abi;
@@ -74,6 +85,31 @@
                     .then(result => {
                         console.log(result.toString());
                     });
+            },
+            async convertToBuffer(reader) {
+                return Buffer.from(reader);
+            },
+            captureFile(file) {
+                console.log(file);
+                const reader = new FileReader();
+                if (typeof file !== 'undefined') {
+                    reader.readAsArrayBuffer(file.target.files[0]);
+                    reader.onloadend = async () => {
+                        this.buffer = await this.convertToBuffer(reader.result);
+                    };
+                }
+                else {
+                    this.buffer = '';
+                }
+            },
+            uploadPhoto: function () {
+                console.log(this.buffer);
+                ipfs.add(this.buffer)
+                    .then(hashedImg => {
+                        let imgHash = hashedImg[0].hash;
+                        console.log(imgHash);
+                        console.log(`https://ipfs.io/ipfs/${imgHash}`);
+                    })
             }
         }
     }
